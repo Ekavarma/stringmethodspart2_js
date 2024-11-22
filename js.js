ba95be0aa0da3956439c40ba4da9c82f3a05302e
Ekavarma Petla <ekavarmap@gmail.com>
2024-11-22 23:31:48 +0530

    stringmethodspart2 in javascript

---
+++ js.js
@@ -0,0 +1,36 @@
+//Relace the substring “World!” with “My Dear Friend” ..
+//Write the program for that
+
+ let str = "Hello World!";
+ let str2=str.replace("world","My Dear Friend")
+ console.log(str2)
+
+
+
+//Repeat the “Hungry” for 10 times
+//Write the program for that
+let a="hungry".repeat(10);
+console.log(a)
+
+
+
+//Find the last index of the string “fox”
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+
+let index=sentence.lastIndexOf("fox")
+console.log(index)
+
+//Find the string starts with “hi”
+//Write code for that
+let message = "Hello, world!";
+let message1=message.startsWith("hi")
+console.log(message1)
+
+//Find the string ends with “.pdf”
+//Write code for that
+let fileName = "document.pdf";
+let file=fileName.endsWith(".pdf")
+console.log(file)
+
+
+
